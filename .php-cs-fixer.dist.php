1b2e0737b6eb113d82730acd41dd91bf230b244b
Nozarashi <15169250+nozarashi20@users.noreply.github.com>
2026-08-19 19:55:18 +0200

    chore: bootstrap Symfony application

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,25 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+    ->notPath([
+        'config/bundles.php',
+        'config/reference.php',
+    ])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        'no_useless_else' => true,
+        'yoda_style' => false,
+        'no_useless_return' => true,
+        'concat_space' => ['spacing' => 'one'],
+        'declare_strict_types' => true,
+    ])
+    ->setRiskyAllowed(true)
+    ->setUsingCache(true)
+    ->setFinder($finder)
+;
